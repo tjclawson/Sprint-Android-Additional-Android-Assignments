bafb44d0f433039f165d6a542c076ca4fb8953a8
tjclawson <tjamesclawson@gmail.com>
2019-10-07 15:46:01 -0400

    Add video and button functionality

--- M01/Assignment/app/src/main/java/com/example/mediaservices/MainActivity.kt
+++ M01/Assignment/app/src/main/java/com/example/mediaservices/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.example.mediaservices
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_pause_play.isEnabled = false
+        playPauseButtonInit()
+        Log.i("BIGBRAIN", "${R.raw.live_views_of_starman}")
+
+    }
+
+    private fun playPauseButtonInit() {
+
+        button_pause_play.setOnClickListener {
+            if (vv_main.isPlaying) {
+                vv_main.pause()
+                button_pause_play.text = getString(R.string.play)
+            } else {
+                vv_main.start()
+                button_pause_play.text = getString(R.string.pause)
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        //setting video URI
+        vv_main.setVideoURI(Uri.parse("android.resource://" + packageName + "/" + R.raw.live_views_of_starman))
+
+        //setting on prepared listener
+        vv_main.setOnPreparedListener {
+            button_pause_play.isEnabled = true
+            it.let {
+                seekbar.max = it.duration
+            }
+        }
     }
 }

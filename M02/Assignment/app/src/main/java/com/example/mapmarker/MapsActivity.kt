b97dcd5624b8f91e5458791471e33352a7284efe
tjclawson <tjamesclawson@gmail.com>
2019-10-08 19:03:39 -0400

    MapMarker Assignment Complete

--- M02/Assignment/app/src/main/java/com/example/mapmarker/MapsActivity.kt
+++ M02/Assignment/app/src/main/java/com/example/mapmarker/MapsActivity.kt
@@ -1,7 +1,15 @@
 package com.example.mapmarker
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,18 +17,26 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private val FINE_LOCATION_REQUEST_CODE = 5
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    lateinit var currrentLocation: Location
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        button_center.isEnabled = false
+        button_place_marker.isEnabled = false
+        getCurrentLocation()
     }
 
     /**
@@ -35,9 +51,52 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        button_center.setOnClickListener {
+            var currentLatLng = LatLng(currrentLocation.latitude, currrentLocation.longitude)
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng))
+        }
+
+        button_place_marker.setOnClickListener {
+            var currentLatLng = LatLng(currrentLocation.latitude, currrentLocation.longitude)
+            mMap.addMarker(MarkerOptions().position(currentLatLng).title("Marker in Sydney"))
+        }
+    }
+
+    private fun getLocation() {
+        var currentLocationResult: Location? = null
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+
+        val locationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        locationProviderClient.lastLocation.addOnSuccessListener { location ->
+            currentLocationResult = location
+            if (currentLocationResult != null) {
+                currrentLocation = currentLocationResult as Location
+                button_center.isEnabled = true
+                button_place_marker.isEnabled = true
+            } else {
+                Toast.makeText(this, "Request Failed Maybe", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
+    private fun getCurrentLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), FINE_LOCATION_REQUEST_CODE)
+        } else {
+            getLocation()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == FINE_LOCATION_REQUEST_CODE)  {
+            if (permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLocation()
+            }
+        }
     }
 }
